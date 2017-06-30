ee458433aaab87184f27300e38de412d34ef4731
makaer15 <mahmutkaan.eryilmaz@agu.edu.tr>
2017-06-30 14:18:52 +0300

    q3

--- 3.py
+++ 3.py
@@ -1,1 +1,7 @@
-print("to be continued.")
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+sol = []
+x = int(input("Number to be compared: "))
+for m in a:
+    if m > x:
+        sol.append(m)
+print(sol)
