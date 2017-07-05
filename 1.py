7df7d6114ad5828a453af8529b1b841840a3a692
mkaanery <mahmutkaan.eryilmaz@gmail.com>
2017-07-05 08:31:23 +0300

    testing git

--- 1.py
+++ 1.py
@@ -1,3 +1,3 @@
 name = input('What is your name: ')
 age = int(input('What is your age: '))
-print(name + ', you will be ' + ' 100 years old in ' + str((2017 - age + 100)))
+print(name + ', you will be' + ' 100 years old in ' + str((2017 - age + 100)))
