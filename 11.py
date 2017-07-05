6f9d5f2bc350340a596fe7dc91dc7399c88ea2a2
mkaanery <mahmutkaan.eryilmaz@agu.edu.tr>
2017-07-05 17:21:15 +0300

    q12 update

--- 11.py
+++ 11.py
@@ -1,12 +1,15 @@
-def getNumber(text = "Number: "):
+def getNumber(text="Number: "):
     return int(input(text))
 
-num = getNumber("Enter the number whose primality will be found: ")
-
 def isPrime(number):
-    for m in range(number - 1, 1, -1):
+    if number == 1:
+        return False
+    elif number == 2:
+        return True
+    for m in range(int(number / 2) + 1, 1, -1):
         if (number % m == 0):
             return False
     return True
 
-print("Primality: " + str(isPrime(num)))
+print("Primality: " +
+      str(isPrime(getNumber("Enter the number whose primality will be found: "))))
---
+++ 12.py
@@ -0,0 +1,7 @@
+import random
+
+a = random.sample(range(random.randint(100, 1000)), random.randint(10, 100))
+
+print(a)
+print(a[0:1])
+print(a[-1:])
